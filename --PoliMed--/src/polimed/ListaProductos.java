fd5156f4fbd436f5b2ba921316419a5c76408d97
dansebcat <danielcfell@hotmail.com>
2020-08-30 17:20:20 -0500

    Revert "Revert """
    This reverts commit f52c9c4f287ac140876096052c2e59db8b981d36.

---
+++ --PoliMed--/src/polimed/ListaProductos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimed;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author danie
+ */
+public class ListaProductos {
+    private ArrayList <Producto> listaProductos;
+
+    public ListaProductos() {
+        listaProductos= new ArrayList<Producto>();
+        
+    }
+    public void añadirProducto(Producto producto){
+        listaProductos.add(producto);
+    }
+    
+}
